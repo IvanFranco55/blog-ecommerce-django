cd2ecaadf6c778061f2c5de4158b6b67ba382d02
ivan <ivanfranco4382@gmail.com>
2025-12-03 16:46:05 -0300

    Agregamos una clase para que el usuario tenga un perfil

--- repositorioecommerce/ecommerce/apps/blog/models.py
+++ repositorioecommerce/ecommerce/apps/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+# Esta tabla extiende al usuario de Django para agregarle datos extra
+class Perfil(models.Model):
+    # Relacionamos con el usuario base de Django
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='usuarios/', default='usuarios/user_default.png', null=True, blank=True)
+    
+    def __str__(self):
+        return f"Perfil de {self.usuario.username}"
 # Create your models here.
