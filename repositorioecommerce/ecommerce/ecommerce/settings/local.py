0aafc02a886606dbbb75f8dc8da6ba601b315d6f
ivan <ivanfranco4382@gmail.com>
2025-12-10 12:19:12 -0300

    .

--- repositorioecommerce/ecommerce/ecommerce/settings/local.py
+++ repositorioecommerce/ecommerce/ecommerce/settings/local.py
@@ -2,9 +2,9 @@ from .base import *
 
 SECRET_KEY = 'django-insecure-u8kz=^36%+p**-##k2m$2s@u%o&!xlld$%9+rd8)a(fx(spl9s'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
